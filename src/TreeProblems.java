bdaa44840d4df101c5e93dab30bec617ed2d272c
BushyMustache <205280148+BushyMustache@users.noreply.github.com>
2026-09-24 12:24:35 -0700

    Completed sumTree (Map Version) method and its tests

--- src/TreeProblems.java
+++ src/TreeProblems.java
@@ -120,7 +120,12 @@ public class TreeProblems {
    Hint: There's a simple way to do this!
   */
   public static int sumTree(Map<Integer, List<Integer>> tree) {
-    return -1;
+    if (tree == null) return 0;
+    int total = 0;
+
+    for (int node : tree.keySet()) total += node;
+
+    return total;
   }
 
   /*
